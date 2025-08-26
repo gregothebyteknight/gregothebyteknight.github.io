56e2d23f0f4211e013da55d10e41de6bbde16ec8
gregothebyteknight <platypusinthebus@gmail.com>
2025-08-26 20:11:57 +0200

    adding supplemetary files

---
+++ scripts.js
@@ -0,0 +1,9 @@
+sh();
+function sh() {
+    info = document.getElementById("info");
+    button = document.getElementById("button");
+    if (info.style.display == "none") {
+        info-style.display == "block";
+        button-style.display == "none"
+    }
+}
